46f3b4904a1a29efeb602acf408bf7effa7ce3da
Alvaro Rumpel <alvaroRumpel@gmail.com>
2021-12-29 18:34:17 -0300

    update config

--- api/config/config.js
+++ api/config/config.js
@@ -16,8 +16,8 @@ module.exports = {
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DEV_DB_DIALECT
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOSTNAME,
+    "dialect": "postgres"
   }
 }

b04f19eed8f80ad0655fcbc385ae606ede1ca349
Alvaro Rumpel <alvaroRumpel@gmail.com>
2021-12-29 18:07:04 -0300

    update configs

---
+++ api/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": process.env.DEV_DB_USERNAME,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB_DATABASE,
+    "host": process.env.DEV_DB_HOST,
+    "dialect": process.env.DEV_DB_DIALECT
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": process.env.DEV_DB_DIALECT
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DEV_DB_DIALECT
+  }
+}

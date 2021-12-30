d057ea3ddba4181391fced688bd404ff666c57ca
Alvaro Rumpel <alvaroRumpel@gmail.com>
2021-12-30 15:13:53 -0300

    update emailController

--- api/controllers/EmailController.js
+++ api/controllers/EmailController.js
@@ -1,13 +1,15 @@
 const nodemailer = require('nodemailer')
 
-const emailProductionSettings = {
-  host: process.env.EMAIL_HOST,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  },
-  secure: true
-}
+const emailProductionSettings = () => (
+  {
+    host: process.env.EMAIL_HOST,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
+    },
+    secure: true
+  }
+)
 
 const emailTestSettings = (testAccount) => (
   {
